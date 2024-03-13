922af1f4e7fd9536ef38cc8c2fbcdee8af2e0fba
Snizhana Kostiuk <snizhana2812kostyuk@gmail.com>
2024-03-13 14:33:51 +0200

    dev update [UsersTables]

--- Models/User.cs
+++ Models/User.cs
@@ -3,15 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
-namespace EntityApp_1.Models;
-class User
+namespace EntityApp_1.Models 
 {
-    /*Створити WPF проект в якому з допомогою EF Core створити базу даних
-    з табличкою користувачів. Колонки: id, name, login, password*/
+    [Table("UsersTable")]
 
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public string Login { get; set; }
-    public string Password { get; set; }
+    public class User
+    {
+        /*Створити WPF проект в якому з допомогою EF Core створити базу даних
+        з табличкою користувачів. 
+        UsersTable
+        - Id primary key
+        - Name nvarchar(64) not null
+        - Login nvarchar(32) not null
+        - Password nvarchar(32) not null*/
+        [Key]
+        public int Id { get; set; }
+    
+        [Required]
+        [StringLength(64)]
+        public string Name { get; set; }
+    
+        [Required]
+        [StringLength(32)]
+        public string Login { get; set; }
+    
+        [Required]
+        [StringLength(32)]
+        public string Password { get; set; }
+    }
 }
